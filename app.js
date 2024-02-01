f5413ab2e901146ed6e7f325606dd42667e5853c
kdevkh <kdevkh@gmail.com>
2024-02-01 15:22:30 +0900

    Rollback: session rolledback to token

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ import ResumesRouter from "./routers/resumes.router.js";
 import CommentsRouter from "./routers/comments.router.js";
 import LogMiddleware from "./middlewares/log.middleware.js";
 import errorHandlingMiddleware from "./middlewares/error-handling.middleware.js";
-import expressSession from "express-session";
-import expressMySQLSession from "express-mysql-session";
+// import expressSession from "express-session";
+// import expressMySQLSession from "express-mysql-session";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -14,32 +14,10 @@ dotenv.config();
 const app = express();
 const PORT = 3018;
 
-const MySQLStore = expressMySQLSession(expressSession);
-const sessionStore = new MySQLStore({
-  user: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE_NAME,
-  expiration: 1000 * 60 * 60 * 24, // 세션의 만료 기간을 1일로 설정합니다.
-  createDatabaseTable: true, // 세션 테이블을 자동으로 생성합니다.
-});
-
 app.use(LogMiddleware);
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  expressSession({
-    secret: process.env.SESSION_SECRET_KEY, // 세션을 암호화하는 비밀 키를 설정
-    resave: false, // 클라이언트의 요청이 올 때마다 세션을 새롭게 저장할 지 설정, 변경사항이 없어도 다시 저장
-    saveUninitialized: false, // 세션이 초기화되지 않았을 때 세션을 저장할 지 설정
-    cookie: {
-      // 세션 쿠키 설정
-      maxAge: 1000 * 60 * 60 * 24, // 쿠키의 만료 기간을 1일로 설정합니다.
-    },
-    store: sessionStore,
-  }),
-);
+
 app.use("/api", [UsersRouter, ResumesRouter, CommentsRouter]);
 app.use(errorHandlingMiddleware);
 
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,14 +1,22 @@
-import jwt from "jsonwebtoken"; // jwt 라이브러리 사용
+import jwt from "jsonwebtoken";
 import { prisma } from "../models/index.js"; // Prisma client 가져오기
 
 export default async function (req, res, next) {
   try {
-    const { userId } = req.session;
-    if (!userId) throw new Error("로그인이 필요합니다.");
+    const { authorization } = req.cookies;
+    if (!authorization) throw new Error("토큰이 존재하지 않습니다.");
+
+    const [tokenType, token] = authorization.split(" ");
+
+    if (tokenType !== "Bearer")
+      throw new Error("토큰 타입이 일치하지 않습니다.");
+
+    const decodedToken = jwt.verify(token, "custom-secret-key");
+    const userId = decodedToken.userId;
 
     const user = await prisma.users.findFirst({
       where: { userId: +userId },
-    }); // 유저 실제 조회
+    });
     if (!user) {
       res.clearCookie("authorization");
       throw new Error("토큰 사용자가 존재하지 않습니다.");
@@ -19,8 +27,18 @@ export default async function (req, res, next) {
 
     next();
   } catch (error) {
-    return res
-      .status(400)
-      .json({ message: error.message ?? "비정상적인 요청입니다." });
+    res.clearCookie("authorization");
+
+    // 토큰이 만료되었거나, 조작되었을 때, 에러 메시지를 다르게 출력합니다.
+    switch (error.name) {
+      case "TokenExpiredError":
+        return res.status(401).json({ message: "토큰이 만료되었습니다." });
+      case "JsonWebTokenError":
+        return res.status(401).json({ message: "토큰이 조작되었습니다." });
+      default:
+        return res
+          .status(401)
+          .json({ message: error.message ?? "비정상적인 요청입니다." });
+    }
   }
 }
